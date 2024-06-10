633eb1f339c76ac479932035c3cb7948fc8957ff
Sergio Rodriguez <sergiomorales9510@gmail.com>
2024-06-10 11:36:28 -0500

    update

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,26 +1,33 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../Context/ShopContext'
+import React, { useContext, useState } from 'react';
+import { ShopContext } from '../Context/ShopContext';
 import { dropdown_icon } from '../assets/index';
-import Item from '../components/Item/Item'
+import Item from '../components/Item/Item';
 
 const ShopCategory = (props) => {
+  const { all_products } = useContext(ShopContext);
+  const [currentIndex, setCurrentIndex] = useState(12);
 
-  const { all_products } = useContext(ShopContext)
+  const handleExploreMore = () => {
+    setCurrentIndex(currentIndex + 12);
+  };
+
+  const hasMoreProducts = all_products.length > currentIndex;
 
   return (
-    <div >
-      <img className='block mx-auto w-full' src={props.banner} alt="" />
-      <div className="flex  sm:mx-20 md:mx-20 lg:mx-18 xl:mx-18 2xl:mx-20 mt-4 md:mt-0 justify-between items-center text-xs sm:text-xs md:text-base">
+    <div>
+      <img className="block mx-auto w-full" src={props.banner} alt="" />
+      <div className="flex sm:mx-20 md:mx-20 lg:mx-18 xl:mx-18 2xl:mx-20 mt-4 md:mt-0 justify-between items-center text-xs sm:text-xs md:text-base">
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>Showing 1-{currentIndex}</span> out of {all_products.length} products
         </p>
         <button className="flex items-center p-1 md:p-1 md:px-4 rounded-full border border-gray-400">
           Sort by <img className="ml-2" src={dropdown_icon} alt="" />
         </button>
       </div>
       <div className="flex flex-wrap justify-center mx-auto w-full">
-        {
-          all_products.map((item, i) => {
+        {all_products
+          .slice(0, currentIndex)
+          .map((item, i) => {
             if (item.category === props.category) {
               return (
                 <Item
@@ -30,19 +37,24 @@ const ShopCategory = (props) => {
                   image={item.image}
                   new_price={item.new_price}
                   old_price={item.old_price}
-                  description={item.description} />
-              )
+                  description={item.description}
+                />
+              );
             } else {
-              return null
+              return null;
             }
-          })
-        }
+          })}
       </div>
-      <button className="flex justify-center items-center mx-auto my-2 w-48 h-16 rounded-full bg-gray-200 text-gray-500 text-sm font-medium">
-        Explore More
-      </button>
+      {hasMoreProducts && (
+        <button
+          onClick={handleExploreMore}
+          className="flex justify-center items-center mx-auto my-1 w-48 h-16 rounded-xl bg-red-600 text-white text-lg font-medium"
+        >
+          Explore More
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
