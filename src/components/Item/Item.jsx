24e8fef2877e73984b7e1fefbd50e26c5ff7241e
Sergio Rodriguez <sergiomorales9510@gmail.com>
2024-06-10 13:04:52 -0500

    update

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from "framer-motion"
 
 const Item = (props) => {
   return (
-    <div className="flex flex-col  items-start sm:w-1/2 md:w-1/3 lg:w-1/5 mb-4  m-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+    <motion.div 
+      className="flex flex-col items-start sm:w-1/2 md:w-1/3 lg:w-1/5 mb-4 m-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ delay: 0.1, duration: 0.1 }}
+    >
       <Link to={`/product/${props.id}`} className="w-full px-0">
         <img onClick={() => window.scrollTo(0, 0)} src={props.image} alt={props.name} className="w-full h-100 object-center" />
       </Link>
@@ -13,7 +19,7 @@ const Item = (props) => {
         <p className="text-red-500 font-semibold">${props.new_price}</p>
         <p className="text-gray-500 line-through">${props.old_price}</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
