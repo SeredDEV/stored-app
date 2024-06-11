4274b1d54abdca0d1d1a855145beaf0d62a2be68
Sergio Rodriguez <sergiomorales9510@gmail.com>
2024-06-10 19:29:22 -0500

    update

--- src/assets/all_product.js
+++ src/assets/all_product.js
@@ -36,6 +36,80 @@ import siames6_img from "./Product/cats/siames6.png";
 import siames7_img from "./Product/cats/siames7.png";
 import siames8_img from "./Product/cats/siames8.png";
 import siames9_img from "./Product/cats/siames9.png";
+import Bulldog1 from "./Product/dogs/Bulldog1.png";
+import Bulldog2 from "./Product/dogs/Bulldog2.png";
+import Bulldog3 from "./Product/dogs/Bulldog3.png";
+import Bulldog4 from "./Product/dogs/Bulldog4.png";
+import Bulldog5 from "./Product/dogs/Bulldog5.png";
+import Bulldog6 from "./Product/dogs/Bulldog6.png";
+import Bulldog7 from "./Product/dogs/Bulldog7.png";
+import Bulldog8 from "./Product/dogs/Bulldog8.png";
+import Bulldog9 from "./Product/dogs/Bulldog9.png";
+import Bulldog10 from "./Product/dogs/Bulldog10.png";
+import Bulldog11 from "./Product/dogs/Bulldog11.png";
+import cavalier1 from "./Product/dogs/cavalier1.png";
+import cavalier2 from "./Product/dogs/cavalier2.png";
+import cavalier3 from "./Product/dogs/cavalier3.png";
+import cavalier4 from "./Product/dogs/cavalier4.png";
+import cavalier5 from "./Product/dogs/cavalier5.png";
+import cavalier6 from "./Product/dogs/cavalier6.png";
+import cavalier7 from "./Product/dogs/cavalier7.png";
+import cavalier8 from "./Product/dogs/cavalier8.png";
+import cavalier9 from "./Product/dogs/cavalier9.png";
+import cavalier10 from "./Product/dogs/cavalier10.png";
+import cavalier11 from "./Product/dogs/cavalier11.png";
+import cavalier12 from "./Product/dogs/cavalier12.png";
+import cavalier13 from "./Product/dogs/cavalier13.png";
+import cavalier14 from "./Product/dogs/cavalier14.png";
+import cavalier15 from "./Product/dogs/cavalier15.png";
+import GermanShepherd1 from "./Product/dogs/GermanShepherd1.png";
+import GermanShepherd2 from "./Product/dogs/GermanShepherd2.png";
+import GermanShepherd3 from "./Product/dogs/GermanShepherd3.png";
+import GermanShepherd4 from "./Product/dogs/GermanShepherd4.png";
+import GermanShepherd5 from "./Product/dogs/GermanShepherd5.png";
+import GermanShepherd6 from "./Product/dogs/GermanShepherd6.png";
+import GermanShepherd7 from "./Product/dogs/GermanShepherd7.png";
+import GermanShepherd8 from "./Product/dogs/GermanShepherd8.png";
+import GermanShepherd9 from "./Product/dogs/GermanShepherd9.png";
+import GermanShepherd10 from "./Product/dogs/GermanShepherd10.png";
+import GermanShepherd11 from "./Product/dogs/GermanShepherd11.png";
+import GermanShepherd12 from "./Product/dogs/GermanShepherd12.png";
+import GermanShepherd13 from "./Product/dogs/GermanShepherd13.png";
+import GermanShepherd14 from "./Product/dogs/GermanShepherd14.png";
+import GermanShepherd15 from "./Product/dogs/GermanShepherd15.png";
+import GermanShepherd16 from "./Product/dogs/GermanShepherd16.png";
+import GermanShepherd17 from "./Product/dogs/GermanShepherd17.png";
+import GermanShepherd18 from "./Product/dogs/GermanShepherd18.png";
+import Husky1 from "./Product/dogs/Husky1.png";
+import Husky2 from "./Product/dogs/Husky2.png";
+import Husky3 from "./Product/dogs/Husky3.png";
+import Husky4 from "./Product/dogs/Husky4.png";
+import Husky5 from "./Product/dogs/Husky5.png";
+import Husky6 from "./Product/dogs/Husky6.png";
+import Husky7 from "./Product/dogs/Husky7.png";
+import Husky8 from "./Product/dogs/Husky8.png";
+import Husky9 from "./Product/dogs/Husky9.png";
+import Husky10 from "./Product/dogs/Husky10.png";
+import Husky11 from "./Product/dogs/Husky11.png";
+import Husky12 from "./Product/dogs/Husky12.png";
+import Husky13 from "./Product/dogs/Husky13.png";
+import Husky14 from "./Product/dogs/Husky14.png";
+import Husky15 from "./Product/dogs/Husky15.png";
+import Husky16 from "./Product/dogs/Husky16.png";
+import Husky17 from "./Product/dogs/Husky17.png";
+import Husky18 from "./Product/dogs/Husky18.png";
+import retriever1 from "./Product/dogs/retriever1.png";
+import retriever2 from "./Product/dogs/retriever2.png";
+import retriever3 from "./Product/dogs/retriever3.png";
+import retriever4 from "./Product/dogs/retriever4.png";
+import retriever5 from "./Product/dogs/retriever5.png";
+import retriever6 from "./Product/dogs/retriever6.png";
+import retriever7 from "./Product/dogs/retriever7.png";
+import retriever8 from "./Product/dogs/retriever8.png";
+import retriever9 from "./Product/dogs/retriever9.png";
+import retriever10 from "./Product/dogs/retriever10.png";
+import retriever11 from "./Product/dogs/retriever11.png";
+import retriever12 from "./Product/dogs/retriever12.png";
 
 
 
@@ -381,6 +455,672 @@ let all_product = [
     new_price: 80.0,
     old_price: 110.5,
     description: "Siamese cat with a sleek coat."
+  },
+  {
+    id: 39,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog1,
+    new_price: 50.0,
+    old_price: 80.5,
+    description: "Bull dog enjoying the sunshine."
+  },
+  {
+    id: 40,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog2,
+    new_price: 45.0,
+    old_price: 70.0,
+    description: "Bulldog playing in the park."
+  },
+  {
+    id: 41,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog3,
+    new_price: 55.0,
+    old_price: 85.0,
+    description: "Adorable Bulldog with a toy."
+  },
+  {
+    id: 42,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog4,
+    new_price: 52.0,
+    old_price: 82.5,
+    description: "Bulldog taking a nap."
+  },
+  {
+    id: 43,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog5,
+    new_price: 48.0,
+    old_price: 78.0,
+    description: "Bulldog with a funny hat."
+  },
+  {
+    id: 44,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog6,
+    new_price: 49.0,
+    old_price: 79.5,
+    description: "Cute Bulldog looking curious."
+  },
+  {
+    id: 45,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog7,
+    new_price: 51.0,
+    old_price: 81.0,
+    description: "Happy Bulldog wagging its tail."
+  },
+  {
+    id: 46,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog8,
+    new_price: 50.5,
+    old_price: 80.0,
+    description: "Bulldog enjoying a treat."
+  },
+  {
+    id: 47,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog9,
+    new_price: 47.0,
+    old_price: 77.5,
+    description: "Bulldog sitting in the garden."
+  },
+  {
+    id: 48,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog10,
+    new_price: 46.0,
+    old_price: 76.5,
+    description: "Bulldog with a happy face."
+  },
+  {
+    id: 49,
+    name: "Bulldog",
+    category: "dog",
+    image: Bulldog11,
+    new_price: 53.0,
+    old_price: 83.0,
+    description: "Bulldog running on the beach."
+  },
+  {
+    id: 50,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier1,
+    new_price: 55.0,
+    old_price: 85.0,
+    description: "Cavalier enjoying a sunny day."
+  },
+  {
+    id: 51,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier2,
+    new_price: 50.0,
+    old_price: 80.5,
+    description: "Cavalier playing in the grass."
+  },
+  {
+    id: 52,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier3,
+    new_price: 60.0,
+    old_price: 90.0,
+    description: "Cavalier with a ball."
+  },
+  {
+    id: 53,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier4,
+    new_price: 57.0,
+    old_price: 87.5,
+    description: "Cavalier resting on the sofa."
+  },
+  {
+    id: 54,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier5,
+    new_price: 54.0,
+    old_price: 84.0,
+    description: "Cute Cavalier with a toy."
+  },
+  {
+    id: 55,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier6,
+    new_price: 58.0,
+    old_price: 88.0,
+    description: "Happy Cavalier wagging its tail."
+  },
+  {
+    id: 56,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier7,
+    new_price: 53.0,
+    old_price: 83.0,
+    description: "Cavalier taking a nap."
+  },
+  {
+    id: 57,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier8,
+    new_price: 59.0,
+    old_price: 89.0,
+    description: "Cavalier with a cute outfit."
+  },
+  {
+    id: 58,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier9,
+    new_price: 56.0,
+    old_price: 86.0,
+    description: "Cavalier enjoying a treat."
+  },
+  {
+    id: 59,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier10,
+    new_price: 52.0,
+    old_price: 82.0,
+    description: "Cavalier running in the park."
+  },
+  {
+    id: 60,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier11,
+    new_price: 51.0,
+    old_price: 81.0,
+    description: "Cavalier with a happy face."
+  },
+  {
+    id: 61,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier12,
+    new_price: 55.5,
+    old_price: 85.5,
+    description: "Cavalier sitting calmly."
+  },
+  {
+    id: 62,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier13,
+    new_price: 57.5,
+    old_price: 87.5,
+    description: "Cavalier in the garden."
+  },
+  {
+    id: 63,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier14,
+    new_price: 54.5,
+    old_price: 84.5,
+    description: "Cavalier with a playful mood."
+  },
+  {
+    id: 64,
+    name: "Cavalier",
+    category: "dog",
+    image: cavalier15,
+    new_price: 59.5,
+    old_price: 89.5,
+    description: "Cavalier enjoying the outdoors."
+  },
+  {
+    id: 65,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd1,
+    new_price: 60.0,
+    old_price: 90.0,
+    description: "German Shepherd enjoying the park."
+  },
+  {
+    id: 66,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd2,
+    new_price: 62.0,
+    old_price: 92.5,
+    description: "German Shepherd with a ball."
+  },
+  {
+    id: 67,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd3,
+    new_price: 65.0,
+    old_price: 95.0,
+    description: "German Shepherd on a walk."
+  },
+  {
+    id: 68,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd4,
+    new_price: 61.0,
+    old_price: 91.0,
+    description: "German Shepherd playing fetch."
+  },
+  {
+    id: 69,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd5,
+    new_price: 63.0,
+    old_price: 93.5,
+    description: "Happy German Shepherd running."
+  },
+  {
+    id: 70,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd6,
+    new_price: 64.0,
+    old_price: 94.0,
+    description: "German Shepherd in the garden."
+  },
+  {
+    id: 71,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd7,
+    new_price: 66.0,
+    old_price: 96.0,
+    description: "German Shepherd resting on the lawn."
+  },
+  {
+    id: 72,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd8,
+    new_price: 67.0,
+    old_price: 97.0,
+    description: "German Shepherd with a toy."
+  },
+  {
+    id: 73,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd9,
+    new_price: 68.0,
+    old_price: 98.0,
+    description: "German Shepherd sitting proudly."
+  },
+  {
+    id: 74,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd10,
+    new_price: 69.0,
+    old_price: 99.0,
+    description: "German Shepherd running in the field."
+  },
+  {
+    id: 75,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd11,
+    new_price: 70.0,
+    old_price: 100.0,
+    description: "German Shepherd with a playful mood."
+  },
+  {
+    id: 76,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd12,
+    new_price: 71.0,
+    old_price: 101.0,
+    description: "German Shepherd enjoying the view."
+  },
+  {
+    id: 77,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd13,
+    new_price: 72.0,
+    old_price: 102.0,
+    description: "German Shepherd by the lake."
+  },
+  {
+    id: 78,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd14,
+    new_price: 73.0,
+    old_price: 103.0,
+    description: "German Shepherd exploring the woods."
+  },
+  {
+    id: 79,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd15,
+    new_price: 74.0,
+    old_price: 104.0,
+    description: "German Shepherd running on the beach."
+  },
+  {
+    id: 80,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd16,
+    new_price: 75.0,
+    old_price: 105.0,
+    description: "German Shepherd with a joyful expression."
+  },
+  {
+    id: 81,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd17,
+    new_price: 76.0,
+    old_price: 106.0,
+    description: "German Shepherd playing with friends."
+  },
+  {
+    id: 82,
+    name: "Shepherd",
+    category: "dog",
+    image: GermanShepherd18,
+    new_price: 77.0,
+    old_price: 107.0,
+    description: "German Shepherd resting after play."
+  },
+  {
+    id: 83,
+    name: "Husky",
+    category: "dog",
+    image: Husky1,
+    new_price: 65.0,
+    old_price: 95.0,
+    description: "Husky enjoying the snow."
+  },
+  {
+    id: 84,
+    name: "Husky",
+    category: "dog",
+    image: Husky2,
+    new_price: 66.0,
+    old_price: 96.0,
+    description: "Husky with bright blue eyes."
+  },
+  {
+    id: 85,
+    name: "Husky",
+    category: "dog",
+    image: Husky3,
+    new_price: 67.0,
+    old_price: 97.0,
+    description: "Playful Husky in the yard."
+  },
+  {
+    id: 86,
+    name: "Husky",
+    category: "dog",
+    image: Husky4,
+    new_price: 68.0,
+    old_price: 98.0,
+    description: "Husky running in the field."
+  },
+  {
+    id: 87,
+    name: "Husky",
+    category: "dog",
+    image: Husky5,
+    new_price: 69.0,
+    old_price: 99.0,
+    description: "Husky with a happy face."
+  },
+  {
+    id: 88,
+    name: "Husky",
+    category: "dog",
+    image: Husky6,
+    new_price: 70.0,
+    old_price: 100.0,
+    description: "Husky relaxing at home."
+  },
+  {
+    id: 89,
+    name: "Husky",
+    category: "dog",
+    image: Husky7,
+    new_price: 71.0,
+    old_price: 101.0,
+    description: "Husky enjoying a treat."
+  },
+  {
+    id: 90,
+    name: "Husky",
+    category: "dog",
+    image: Husky8,
+    new_price: 72.0,
+    old_price: 102.0,
+    description: "Husky playing with a toy."
+  },
+  {
+    id: 91,
+    name: "Husky",
+    category: "dog",
+    image: Husky9,
+    new_price: 73.0,
+    old_price: 103.0,
+    description: "Husky exploring the garden."
+  },
+  {
+    id: 92,
+    name: "Husky",
+    category: "dog",
+    image: Husky10,
+    new_price: 74.0,
+    old_price: 104.0,
+    description: "Husky with a playful mood."
+  },
+  {
+    id: 93,
+    name: "Husky",
+    category: "dog",
+    image: Husky11,
+    new_price: 75.0,
+    old_price: 105.0,
+    description: "Husky running on the beach."
+  },
+  {
+    id: 94,
+    name: "Husky",
+    category: "dog",
+    image: Husky12,
+    new_price: 76.0,
+    old_price: 106.0,
+    description: "Husky resting after play."
+  },
+  {
+    id: 95,
+    name: "Husky",
+    category: "dog",
+    image: Husky13,
+    new_price: 77.0,
+    old_price: 107.0,
+    description: "Husky with a joyful expression."
+  },
+  {
+    id: 96,
+    name: "Husky",
+    category: "dog",
+    image: Husky14,
+    new_price: 78.0,
+    old_price: 108.0,
+    description: "Husky playing in the snow."
+  },
+  {
+    id: 97,
+    name: "Husky",
+    category: "dog",
+    image: Husky15,
+    new_price: 79.0,
+    old_price: 109.0,
+    description: "Husky enjoying the outdoors."
+  },
+  {
+    id: 98,
+    name: "Husky",
+    category: "dog",
+    image: Husky16,
+    new_price: 80.0,
+    old_price: 110.0,
+    description: "Husky exploring the woods."
+  },
+  {
+    id: 99,
+    name: "Husky",
+    category: "dog",
+    image: Husky17,
+    new_price: 81.0,
+    old_price: 111.0,
+    description: "Husky with a playful smile."
+  },
+  {
+    id: 100,
+    name: "Husky",
+    category: "dog",
+    image: Husky18,
+    new_price: 82.0,
+    old_price: 112.0,
+    description: "Husky running in the garden."
+  },
+  {
+    id: 101,
+    name: "Retriever",
+    category: "dog",
+    image: retriever1,
+    new_price: 70.0,
+    old_price: 100.0,
+    description: "Retriever playing fetch."
+  },
+  {
+    id: 102,
+    name: "Retriever",
+    category: "dog",
+    image: retriever2,
+    new_price: 72.0,
+    old_price: 102.0,
+    description: "Retriever enjoying the sunshine."
+  },
+  {
+    id: 103,
+    name: "Retriever",
+    category: "dog",
+    image: retriever3,
+    new_price: 74.0,
+    old_price: 104.0,
+    description: "Retriever with a happy face."
+  },
+  {
+    id: 104,
+    name: "Retriever",
+    category: "dog",
+    image: retriever4,
+    new_price: 76.0,
+    old_price: 106.0,
+    description: "Retriever running in the park."
+  },
+  {
+    id: 105,
+    name: "Retriever",
+    category: "dog",
+    image: retriever5,
+    new_price: 78.0,
+    old_price: 108.0,
+    description: "Retriever with a playful mood."
+  },
+  {
+    id: 106,
+    name: "Retriever",
+    category: "dog",
+    image: retriever6,
+    new_price: 80.0,
+    old_price: 110.0,
+    description: "Retriever relaxing at home."
+  },
+  {
+    id: 107,
+    name: "Retriever",
+    category: "dog",
+    image: retriever7,
+    new_price: 82.0,
+    old_price: 112.0,
+    description: "Retriever with a joyful expression."
+  },
+  {
+    id: 108,
+    name: "Retriever",
+    category: "dog",
+    image: retriever8,
+    new_price: 84.0,
+    old_price: 114.0,
+    description: "Retriever exploring the garden."
+  },
+  {
+    id: 109,
+    name: "Retriever",
+    category: "dog",
+    image: retriever9,
+    new_price: 86.0,
+    old_price: 116.0,
+    description: "Retriever enjoying a treat."
+  },
+  {
+    id: 110,
+    name: "Retriever",
+    category: "dog",
+    image: retriever10,
+    new_price: 88.0,
+    old_price: 118.0,
+    description: "Retriever running on the beach."
+  },
+  {
+    id: 111,
+    name: "Retriever",
+    category: "dog",
+    image: retriever11,
+    new_price: 90.0,
+    old_price: 120.0,
+    description: "Retriever with a ball."
+  },
+  {
+    id: 112,
+    name: "Retriever",
+    category: "dog",
+    image: retriever12,
+    new_price: 92.0,
+    old_price: 122.0,
+    description: "Retriever playing in the yard."
   }
 
 ];
