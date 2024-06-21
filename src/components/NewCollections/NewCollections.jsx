5083db288068a3cb8e5ffc3cc3d5ca6fe7e3ff4f
Sergio Rodriguez <sergiomorales9510@gmail.com>
2024-06-21 18:54:56 -0500

    update

--- src/components/NewCollections/NewCollections.jsx
+++ src/components/NewCollections/NewCollections.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import new_collection from '../../assets/new_collections'
+import React, { useContext } from 'react'
 import Item from '../Item/Item'
+import { ShopContext } from '../../Context/ShopContext';
 
 const NewCollections = () => {
+
+    const { all_products } = useContext(ShopContext);
+
+    // Filtra y obtiene 4 productos de la categoría "cat"
+    const catProducts = all_products
+        .filter(item => item.category === 'cat')
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 4);
+
+    // Filtra y obtiene 4 productos de la categoría "dog"
+    const dogProducts = all_products
+        .filter(item => item.category === 'dog')
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 4);
+
+    // Combina los productos de ambas categorías
+    const relatedProducts = [...catProducts, ...dogProducts];
+
     return (
         <div className='flex flex-col items-center mt-28 justify-center w-full'>
             <h1 className="text-2xl font-bold mb-4 text-center" style={{ fontSize: '40px' }}>NEW COLLECTIONS</h1>
             <hr className="mb-4" style={{ width: '150px', height: '4px', borderRadius: '10px', background: '#252525' }} />
             <div className='flex flex-wrap items-start justify-center'>
                 {
-                    new_collection.map((item, i) => {
+                    relatedProducts.map((item, i) => {
                         return (
                             <Item
                                 key={i}
