a8eb33b894b328dc1de5cec36d123d5e3a4927bf
Sergio Rodriguez <sergiomorales9510@gmail.com>
2024-06-21 19:26:06 -0500

    update

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -8,8 +8,7 @@ import NewsLetter from '../components/NewsLetter/NewsLetter';
 import useAnimationOnScroll from '../components/hooks/useAnimationOnScroll';
 
 const Shop = () => {
-    const createObserverComponent = (Component) => {
-
+    const createObserverComponent = (Component, props) => {
         const [controls, ref] = useAnimationOnScroll();
 
         return (
@@ -23,7 +22,7 @@ const Shop = () => {
                 }}
                 transition={{ duration: 0.3 }}
             >
-                <Component />
+                <Component {...props} />
             </motion.div>
         );
     };
@@ -31,7 +30,8 @@ const Shop = () => {
     return (
         <div>
             {createObserverComponent(Cartshop)}
-            {createObserverComponent(Popular)}
+            {createObserverComponent(Popular, { ids: [11, 29, 33, 4 ], category: 'cat' })}
+            {createObserverComponent(Popular, { ids: [40, 110, 88, 61], category: 'dog' })}
             {createObserverComponent(Offers)}
             {createObserverComponent(NewCollections)}
             {createObserverComponent(NewsLetter)}
--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import data_product from '../../assets/data'
-import Item from '../Item/Item'
+import React, { useContext } from 'react';
+import { ShopContext } from '../../Context/ShopContext';
+import Item from '../Item/Item';
 
-const Popular = () => {
+const Popular = ({ ids, category }) => {
+  const { all_products } = useContext(ShopContext);
+
+  // Filtrar productos por los IDs especificados
+  const popularProducts = all_products.filter(item => ids.includes(item.id));
+
+  const categoryText = category ? category.toUpperCase() : '';
+
   return (
     <div className='flex flex-col items-center justify-center w-full mt-24'>
-      <h1 className="text-2xl font-bold mb-4 text-center" style={{ fontSize: '40px' }}>POPULAR CAT</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center" style={{ fontSize: '40px' }}>
+        POPULAR {categoryText}
+      </h1>
       <hr className="mb-4" style={{ width: '150px', height: '4px', borderRadius: '10px', background: '#252525' }} />
       <div className='flex flex-wrap items-start justify-center'>
-        {data_product.map((item, i) => {
-          return <Item
+        {popularProducts.map((item, i) => (
+          <Item
             key={i}
             id={item.id}
             name={item.name}
@@ -18,10 +27,10 @@ const Popular = () => {
             old_price={item.old_price}
             description={item.description}
           />
-        })}
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Popular
+export default Popular;
